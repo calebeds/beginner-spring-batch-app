fed7271b7e6316b35b9502eedf85062b9640f9be
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-09-26 21:47:06 -0300

    SensorDataTextMapper

---
+++ src/main/java/me/calebe_oliveira/beginnerspringbatchapp/mapper/SensorDataTextMapper.java
@@ -0,0 +1,19 @@
+package me.calebe_oliveira.beginnerspringbatchapp.mapper;
+
+import me.calebe_oliveira.beginnerspringbatchapp.domain.DailySensorData;
+import org.springframework.batch.item.file.LineMapper;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class SensorDataTextMapper implements LineMapper<DailySensorData> {
+
+    @Override
+    public DailySensorData mapLine(String line, int lineNumber) throws Exception {
+        String[] dateAndMeasurements = line.split(":");
+        return new DailySensorData(dateAndMeasurements[0],
+                Arrays.stream(dateAndMeasurements[1].split(","))
+                        .map(Double::parseDouble)
+                        .collect(Collectors.toList()));
+    }
+}
