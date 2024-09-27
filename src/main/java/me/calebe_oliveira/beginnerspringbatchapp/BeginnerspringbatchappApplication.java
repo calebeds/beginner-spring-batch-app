41d5479f98527a22d48088a4cf9a24d9c8d2d8d2
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-09-26 21:18:49 -0300

    first commit

---
+++ src/main/java/me/calebe_oliveira/beginnerspringbatchapp/BeginnerspringbatchappApplication.java
@@ -0,0 +1,13 @@
+package me.calebe_oliveira.beginnerspringbatchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeginnerspringbatchappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeginnerspringbatchappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/calebe_oliveira/beginnerspringbatchapp/BeginnerspringbatchappApplicationTests.java
@@ -0,0 +1,13 @@
+package me.calebe_oliveira.beginnerspringbatchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginnerspringbatchappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
